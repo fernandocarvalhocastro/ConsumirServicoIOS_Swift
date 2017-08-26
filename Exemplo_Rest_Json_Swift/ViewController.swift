0254c836b44e07e438cd401221019630ea42c428
Usuário Convidado <Guest@L2602MICRO07.local>
2017-08-25 21:49:07 -0300

    ïnicio

--- Exemplo_Rest_Json_Swift/ViewController.swift
+++ Exemplo_Rest_Json_Swift/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var minhaImagem: UIImageView!
+    @IBOutlet weak var local: UILabel!
+    @IBOutlet weak var estado: UILabel!
+    
+    var session: URLSession?
+    
+    
+    @IBAction func exibir(_ sender: Any) {
+        let config = URLSessionConfiguration.default
+        session = URLSession(configuration: config)
+        let url = URL(string: "https://parks-api.herokuapp.com/parks")
+        
+        let task = session?.dataTask(with: url!, completionHandler: { (data, response, error) in
+            //codigo aqui
+            let texto = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            print(texto!)
+        })
+        task?.resume()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
